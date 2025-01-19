382bdf8259ecf2fb2b504ee9f7a16b6e40827a86
Henry <keelechiogbu@gmail.com>
2025-01-19 09:25:58 +0100

    added the main components

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,12 @@ export default function App() {
   // overflow-x-hidden removes those ugly scrollbars
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      {/* <NavBar /> */}
-      {/* <Hero /> */}
-      {/* <About /> */}
-      {/* <Features /> */}
-      {/* <Story /> */}
-      {/* <Contact /> */}
-      <h1>Navbarss</h1>
-      <h1>adding bad header comp to git</h1>
-      <p>Remove navbar</p>
-      <h1>Hero</h1>
-      <p>Remove Hero</p>
-      <h1>About</h1>
-      <p>Remove About</p>
-      <h1>Features</h1>
-      <p>Remove Features</p>
-      <h1>Story</h1>
-      <p>Remove this Story</p>
-      <h1>Contact</h1>
-      <p>Remove Contact</p>
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
       <Footer />
     </main>
   )
